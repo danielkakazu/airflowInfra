41d430907d99876ac3a5f39b7df9d4f05c0295d3
danielkakazu <65232109+danielkakazu@users.noreply.github.com>
2025-09-17 13:43:00 -0300

    values from variables

--- variables.tf
+++ variables.tf
@@ -1,10 +1,11 @@
 variable "app_name" {
   type        = string
   description = "Nome base da aplicação"
+  default     = "airflow"
 }
 
 variable "location" {
   type        = string
   description = "Região do Azure para os recursos"
-  default     = "eastus"
+  default     = "centralus"
 }
