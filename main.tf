0a34ecac3e69ba070eb849258cf4d56eef38232a
danielkakazu <65232109+danielkakazu@users.noreply.github.com>
2025-09-17 13:53:28 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,20 +127,9 @@ resource "azurerm_postgresql_flexible_server" "airflow" {
   sku_name = "Standard_B1ms"
 
   administrator_login          = "pgadmin"
-  administrator_login_password = random_password.postgresql_admin.result
+  administrator_password       = random_password.postgresql_admin.result
 
   storage_mb               = 32768
-  backup_retention_days    = 7
-  auto_grow_enabled        = true
-  geo_redundant_backup_enabled = false
   zone                     = "1"
-
-  high_availability {
-    mode = "Disabled"
-  }
-
-  network {
-    public_network_access_enabled = false
-  }
 }
 
